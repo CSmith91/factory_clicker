b6eff9f3ffb75471aaa4035eabe4225e502ad5fc
Chris Smith <csmith.ex.91@gmail.com>
2024-08-12 11:57:35 +0100

    added increment logic to inputMax, made furnace dropdown only if furnaces on site exist (>0)

--- src/Components/MachineOnSite.jsx
+++ src/Components/MachineOnSite.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react"
 import stoneFurnace from './Images/stone_furnace.png'
 
 const MachineOnSite = ({ itemName, machineName, handleMachineChange, onAlert }) => {
-    const [counter, setCounter] = useState(0) // initialises state
-    
+   
     const getMachineImage  = (machineName) => {
         switch (machineName) {
             case 'Stone Furnace':
@@ -12,16 +11,23 @@ const MachineOnSite = ({ itemName, machineName, handleMachineChange, onAlert }) 
                 return null
         }
     } 
-
     const machineImg = getMachineImage(machineName);
+
+    const [counter, setCounter] = useState(0) // initialises state
 
     const onMachineChange = (action) => {
         handleMachineChange(action, machineName).then((result) =>{
             if (result) {
                 setCounter(prevCounter => {
                     if (action === 'increment') {
+                        setInputMax(prev => {
+                            return prev + 10
+                        })
                         return prevCounter + 1;
                     } else if (action === 'decrement') {
+                        setInputMax(prev => {
+                            return prev - 10
+                        })
                         // Ensure the counter doesn't go below zero or any other condition you want to check
                         return prevCounter > 0 ? prevCounter - 1 : prevCounter;
                     } else {
@@ -35,6 +41,9 @@ const MachineOnSite = ({ itemName, machineName, handleMachineChange, onAlert }) 
         })
     };
 
+    const [currentInput, setCurrentInput] = useState(0);
+    const [inputMax, setInputMax] = useState(0);
+
     return(
         <>
         <div className="machineButtons">
@@ -47,10 +56,20 @@ const MachineOnSite = ({ itemName, machineName, handleMachineChange, onAlert }) 
                 {">"}
             </button>
         </div>
+        {/* Conditionally render the div if counter > 0 */}
+        {counter > 0 && (
         <div>
-            {machineImg && <img src={machineImg} alt={`${machineName} Image`} style={{ width: '32px', height: 'auto' }} />}
-            
+            <div style={{marginBottom: "5%"}}>
+                <button>{"Add " + itemName}</button>
+            </div>
+            <div className="machine-div">
+                {machineImg && <img src={machineImg} alt={`${machineName} Image`} style={{ width: '32px', height: 'auto' }} />}
+                <div className="machine-inputs">
+                    <p>{itemName}: {currentInput} / {inputMax}</p>
+                </div>
+            </div>
         </div>
+         )}
         </>
     )
 }
